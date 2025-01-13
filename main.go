53bedc359b54adf521adbca0adaf0468834c62f9
Rasel Parvez Sanny <raselparvezsanny@gmail.com>
2025-01-13 11:29:30 +0600

    add: applied single responsibility principle to maintain better code quality with func

--- main.go
+++ main.go
@@ -2,26 +2,50 @@ package main
 
 import "fmt"
 
+func welcomeMessage() {
+	fmt.Println("Welcome to the application")
+}
+
+func getUserName() string {
+	var name string
+	fmt.Println("Please enter ur name -")
+	fmt.Scanln(&name)
+	return name
+}
+
+func getTwoNumber() (int, int) {
+
+	var num1 int
+	var num2 int
+	fmt.Println("Enter the first number -")
+	fmt.Scanln(&num1) //& => ampersand
+	fmt.Println("Enter the second number -")
+	fmt.Scanln(&num2)
+	return num1, num2
+}
+
 func add(num1 int, num2 int) int {
 	sum := num1 + num2
 	return sum
 }
 
-func getNumbers(num1 int, num2 int) (int, int) {
-	sum := num1 + num2
-	mul := num1 * num2
-	return sum, mul
+func display(name string, sum int) {
+	fmt.Println("Hello,", name)
+	fmt.Println("Summation =", sum)
 }
 
-func printSomething() {
-	fmt.Println("Education must be free!")
-}
-
-func greetings(name string) {
-	fmt.Println("Hello Habib vai, thanks for the course,", name)
+func farewellMessage() {
+	fmt.Println("Thanks for using the application!")
+	fmt.Println("Goodbye!")
 }
 
 func main() {
-	printSomething()
-	greetings("Sanny")
+	// Print welcome message
+	welcomeMessage()
+	name := getUserName()
+	num1, num2 := getTwoNumber()
+	sum := add(num1, num2)
+	display(name, sum)
+	farewellMessage()
+
 }
